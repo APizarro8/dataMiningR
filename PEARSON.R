8d4d45442d4d93a1696a9b89b4181779bdbc5d51
Alicia Pizarro <43381268+AliciaPiza@users.noreply.github.com>
2019-02-04 21:16:04 +0100

    Update PEARSON.R

--- PEARSON.R
+++ PEARSON.R
@@ -1,15 +1,31 @@
-#############################################################################
-#Descargar librerias
+###################################################################################################
+#
+##LIBRERIAS
 require(corrplot)
 
-#############################   ANALISIS COMPONENTES PRINCIPALES ###################################
+# Start processing
+startTime <- Sys.time()
+cat("Start time", format(startTime),"\n")
 
-##Variables LiDAR a correlacionar
-madrid_10Z1 <- read.csv("D:/TFM_BIOMASA/FUSION/CloudMetrics/CloudMetrics_10Z1_0.15m.csv", sep=";")
+# =================================================================================================
+# ANALISIS DE COMPONENTES PRINCIPALES (ACP)
+# =================================================================================================
+
+##DATOS DE ENTRADA 
+datos <- read.csv("XXXdataXXXX", sep=";")
 dim (madrid_10Z1)
 head (madrid_10Z1)
 
-##Correlacion de variables predictoras continuas
+##CORRELACION DE VARIABLES PREDICTORAS CONTINUAS 
 cor.matr = cor(madrid_10Z1[c(3:29)], method= c("pearson"))
 round (cor.matr, 4)
 write.csv(cor.matr, "D:/TFM_BIOMASA/FUSION/Correlaciones/Pearson_10Z1.csv")
+
+#
+#
+# Calculando tiempo de procesamiento
+timeDiff <- Sys.time() - startTime
+cat("\nEl tiempo de procesamiento es de ", format(timeDiff), "\n")
+
+############################################## FINISHED RUN ########################################
+####################################################################################################
